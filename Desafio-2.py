b0e61a6e6b566bcb1c9a197df78e01493d7d03d3
Murillo Reis <murillo.antonio2004@gmail.com>
2023-07-05 02:24:29 -0300

    Criação do loop for e condicional if
    O loop for está percorrendo a string "frase_inicial" e procurando caracteres repetidos. Caso o caractere não estiver dentro da variável "frase_final", então ele sera adicionado. Caso contrário, não ocorrera nada.

--- Desafio-2.py
+++ Desafio-2.py
@@ -1,3 +1,8 @@
 frase_inicial = "Hello, World!"
 
 frase_final = ""
+
+for index in range(len(frase_inicial)):
+    letra = frase_inicial[index]
+    if letra not in frase_final:
+        frase_final += letra
