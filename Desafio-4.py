5f202ed08c419a16e631531790e2d499578987f7
Murillo Reis <murillo.antonio2004@gmail.com>
2023-07-06 01:29:21 -0300

    Criação do loop for e condicionais
    Foi criado um loop for para percorrer cada caractere da string "frase" e atribuindo a variável "letra".
    
    Depois disso, é utilizado uma estrutura condicional if para verificar se a variável deve_ser_maiusculo é verdadeira e o caractere armazenado em “letra” é diferente de um espaço em branco: " ". Caso as condições forem atendidas, o caractere se torna maiúsculo através do método "upper()" e é atrabuido a variável "nova_frase" e a variável "deve_ser_maiusculo se torna falso.
    
    Caso as condições não forem atendidas, significa que o caractere deve continuar minúsculo e ser armazenado na variável "nova_frase".
    
    A última estrutura condicional "if" verifica se o caractere armazenado em "letra" é um ponto de interrogação ou um ponto final ["?", "."]. Caso for, o valor da variável deve_ser_maiusculo se tornará True, uma vez que sempre após um ponto final ou de um ponto de interrogação, vem uma letra maiúscula. Assim, o loop será percorrido até o último caractere da string “frase”, enquanto a string nova_frase é preenchida.

--- Desafio-4.py
+++ Desafio-4.py
@@ -1,2 +1,13 @@
 frase = "hello. how are you? i'm fine, thank you."
 nova_frase = ""
+deve_ser_maiusculo = True
+
+for letra in frase:
+    if deve_ser_maiusculo and letra != " ":
+        nova_frase += letra.upper()
+        deve_ser_maiusculo = False
+    else:
+        nova_frase += letra
+
+    if letra in ["?", "."]:
+        deve_ser_maiusculo = True
