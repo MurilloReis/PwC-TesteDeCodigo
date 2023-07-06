f17669e5d4496508c0a16d4fa85e7819936bd845
Murillo Reis <murillo.antonio2004@gmail.com>
2023-07-05 22:33:19 -0300

    Elaboração do algoritmo que busca e compara palíndromos
    Foi criado um algoritmo para buscar todos os palíndromos em uma string.
    
    O código utiliza dois loops aninhados para gerar todas as possíveis substrings da string original. A cada iteração, uma substring é criada com base nos índices i e j dos loops.
    
    Em seguida, é verificado se a substring é um palíndromo comparando-a com sua versão invertida usando a condição substring == substring[::-1]. Se a substring for um palíndromo, ela é adicionada a uma lista chamada palindromos e, se for maior do que o maior palíndromo encontrado até o momento, ela se torna o novo maior palíndromo.
    
    Após percorrer todas as substrings possíveis, a função retorna o maior palíndromo encontrado.

--- Desafio-3.py
+++ Desafio-3.py
@@ -2,3 +2,13 @@ def buscar_palindromo(string):
 
     palindromos = []
     maior_palindromo = ""
+
+    for i in range(len(string)):
+        for j in range(i+1, len(string)+1):
+            substring = string[i:j]
+            if substring == substring[::-1]:
+                palindromos.append(substring)
+                if len(substring) > len(maior_palindromo):
+                    maior_palindromo = substring
+
+    return maior_palindromo
