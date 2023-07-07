46c9ac064ffce95a59d1c87eeb9e4fcb3cd5b220
Murillo Reis <murillo.antonio2004@gmail.com>
2023-07-07 19:27:26 -0300

    Elaboração da lógica da função
    Foi inicializada uma lista vazia chamada "lista_simetria_caracteres". Em seguida, itera sobre cada caractere em "caracteres_unicos" usando um loop "for". Para cada caractere, conta quantas vezes ele aparece na string "palavra" usando o método "count()" e calcula o resto da divisão da quantidade de vezes que o caractere aparece por 2 ("% 2"). O resultado (0 ou 1) é adicionado à "lista_simetria_caracteres".

--- Desafio-5.py
+++ Desafio-5.py
@@ -1,2 +1,6 @@
 def verificar_anagrama_de_palindromo(palavra):
     caracteres_unicos = set(palavra)
+
+    lista_simetria_caracteres = []
+    for caracter in caracteres_unicos:
+        lista_simetria_caracteres.append(palavra.count(caracter) % 2)
