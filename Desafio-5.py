9073101f5e6209fec33216de1528665500e5e2e0
Murillo Reis <murillo.antonio2004@gmail.com>
2023-07-07 20:07:09 -0300

    Teste da função
    Foi definida uma variável chamada “teste” com o valor da string "racecar".
    
    Por último, é chamada a função "verificar_anagrama_de_palindromo" com a string dentro de "teste" e é impresso o resultado retornado pela função.
    
    OBS: Ressaltando que o retorno está em formato de string porque no exercício foi pedido que todos os outputs fossem nesse formato. Porém, deixei o retorno em formato booleano em comentário.

--- Desafio-5.py
+++ Desafio-5.py
@@ -5,4 +5,9 @@ def verificar_anagrama_de_palindromo(palavra):
     for caracter in caracteres_unicos:
         lista_simetria_caracteres.append(palavra.count(caracter) % 2)
     
+    # return lista_simetria_caracteres.count(1) <= 1
     return "true" if lista_simetria_caracteres.count(1) <= 1 else "false"
+
+teste = "racecar"
+
+print(verificar_anagrama_de_palindromo(teste))
