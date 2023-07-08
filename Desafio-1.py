bd640ff8493a55c6cf9bcf5843153ff899ebe501
Murillo Reis <murillo.antonio2004@gmail.com>
2023-07-08 00:37:25 -0300

    Transformei em função
    Resolvi transformar o código em função em virtude de tornar o programa mais versátil.

--- Desafio-1.py
+++ Desafio-1.py
@@ -1,3 +1,11 @@
-frase = "hello, World! OpenAI is amazing.".split(" ")
-frase.reverse()
-print(" ".join(frase))
+def inverter_frase(frase):
+
+    frase = frase.split(" ")
+    frase_invertida = frase.reverse()
+    frase_invertida = " ".join(frase)
+
+    return frase_invertida
+
+teste = "hello, World! OpenAI is amazing."
+
+print(inverter_frase(teste))
