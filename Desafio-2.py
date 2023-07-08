af95777083011021bb80d8821706145d70196b2c
Murillo Reis <murillo.antonio2004@gmail.com>
2023-07-08 00:43:46 -0300

    Transformei em função
    Resolvi transformar o código em função em virtude de tornar o programa mais versátil.

--- Desafio-2.py
+++ Desafio-2.py
@@ -1,10 +1,14 @@
+def remover_repeticoes(frase):
+
+    frase_final = ""
+
+    for index in range(len(frase)):
+        letra = frase[index]
+        if letra not in frase_final:
+            frase_final += letra
+            
+    return frase_final
+
 frase_inicial = "Hello, World!"
 
-frase_final = ""
-
-for index in range(len(frase_inicial)):
-    letra = frase_inicial[index]
-    if letra not in frase_final:
-        frase_final += letra
-
-print(frase_final)
+print(remover_repeticoes(frase_inicial))
