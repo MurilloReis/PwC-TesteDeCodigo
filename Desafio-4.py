942cadb80288d4f4144253fa6fc030aaac71cc9d
Murillo Reis <murillo.antonio2004@gmail.com>
2023-07-08 00:51:55 -0300

    Transformei em função
    Resolvi transformar o código em função em virtude de tornar o programa mais versátil.

--- Desafio-4.py
+++ Desafio-4.py
@@ -1,15 +1,20 @@
+def formatar_frase(frase):
+
+    nova_frase = ""
+    deve_ser_maiusculo = True
+
+    for letra in frase:
+        if deve_ser_maiusculo and letra != " ":
+            nova_frase += letra.upper()
+            deve_ser_maiusculo = False
+        else:
+            nova_frase += letra
+
+        if letra in ["?", "."]:
+            deve_ser_maiusculo = True
+
+    return nova_frase
+
 frase = "hello. how are you? i'm fine, thank you."
-nova_frase = ""
-deve_ser_maiusculo = True
 
-for letra in frase:
-    if deve_ser_maiusculo and letra != " ":
-        nova_frase += letra.upper()
-        deve_ser_maiusculo = False
-    else:
-        nova_frase += letra
-
-    if letra in ["?", "."]:
-        deve_ser_maiusculo = True
-
-print(nova_frase)
+print(formatar_frase(frase))
